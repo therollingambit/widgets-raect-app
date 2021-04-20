fdc47c03c8521879317bfd3c7f15e0501cb7b6b5
Ahmad Enayathullah <ahmad.enayathullah@fdmgroup.com>
2021-04-20 08:38:09 +0800

    added manual routing for navigation between widgets

--- src/components/App.js
+++ src/components/App.js
@@ -3,6 +3,8 @@ import Accordion from "./Accordion";
 import Search from "./Search";
 import Dropdown from "./Dropdown";
 import Translate from "./Translate";
+import Route from "./Route";
+import Header from "./Header";
 
 const items = [
   {
@@ -35,9 +37,28 @@ const options = [
 ];
 
 const App = () => {
+  const [selected, setSelected] = useState(options[0]);
+
   return (
     <div>
-      <Translate />
+      <Header />
+      <Route path="/">
+        <Accordion items={items} />
+      </Route>
+      <Route path="/list">
+        <Search />
+      </Route>
+      <Route path="/dropdown">
+        <Dropdown
+          label="Select a colour"
+          options={options}
+          selected={selected}
+          onSelectedChange={setSelected}
+        />
+      </Route>
+      <Route path="/translate">
+        <Translate />
+      </Route>
     </div>
   );
 };
